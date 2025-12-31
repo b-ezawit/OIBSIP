5a98209b3927a21073b7b7f0aeb81b38923ef3f6
b-ezawit <bezawitbiazine@gmail.com>
2025-12-31 04:40:27 -0500

    Add index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+let display = document.querySelector('.display');
+let buttons = document.querySelectorAll('.btn');
+
+let expression = "";
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        let value = btn.getAttribute('data-value');
+
+        // CLEAR
+        if (value === "clear") {
+            expression = "";
+            display.textContent = "0";
+            return;
+        }
+
+        // DELETE
+        if (value === "delete") {
+            expression = expression.slice(0, -1);
+            display.textContent = expression || "0";
+            return;
+        }
+
+        // EQUAL (=)
+        if (value === "=") {
+            try {
+                expression = eval(expression).toString();
+            } catch {
+                expression = "Undefined";
+            }
+            display.textContent = expression;
+            return;
+        }
+
+        // INPUT
+        expression += value;
+        display.textContent = expression;
+    });
+});
